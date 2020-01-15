9c8681a3fd62dcdbbd9edce7ecfabbcc78f404e6
zuranski <azuranski@gmail.com>
2020-01-15 15:16:51 -0500

    added propagation of gaussian tasks/directives tuple

--- autoqchem/gaussian_input_generator.py
+++ autoqchem/gaussian_input_generator.py
@@ -7,10 +7,10 @@ logger = logging.getLogger(__name__)
 class gaussian_input_generator(object):
     """generator of gaussian input files"""
 
-    def __init__(self, molecule):
+    def __init__(self, molecule, directory):
         """configure for this molecule"""
 
-        self.directory = f"{config['gaussian']['work_dir']}/{molecule.fs_name}"
+        self.directory = directory
         self.molecule = molecule
 
         if self.molecule.heavy_elements:
@@ -18,24 +18,27 @@ class gaussian_input_generator(object):
         else:
             self.basis_set = config['gaussian']['light_basis_set']
 
-    def create_gaussian_files(self, workflow=gaussian_workflows.equilibrium):
+    def create_gaussian_files(self, workflow=gaussian_workflows.equilibrium) -> list:
         """write gaussian files for each conformation with defined options"""
 
         # prepare directory for gaussian files
         cleanup_directory_files(self.directory, types=["gjf"])
         os.makedirs(self.directory, exist_ok=True)
 
+        # evaluate workflow
+        tasks = tuple(val(self.basis_set) for val in workflow.value)
+
         # resources configuration
-        n_processors = min(config['gaussian']['max_processors'],
-                           self.molecule.mol.NumAtoms() // config['gaussian']['atoms_per_processor'])
-        ram = n_processors * config['gaussian']['ram_per_processor']
+        n_processors = min(config['slurm']['max_processors'],
+                           self.molecule.mol.NumAtoms() // config['slurm']['atoms_per_processor'])
+        ram = n_processors * config['slurm']['ram_per_processor']
         resource_block = f"%nprocshared={n_processors}\n%Mem={ram}GB\n"
 
         logger.info(f"Generating Gaussian input files for {self.molecule.mol.NumConformers()} conformations.")
 
         for conf_id in range(self.molecule.mol.NumConformers()):
             # set conformer
-            conf_name = f"{self.molecule.name}_conf_{conf_id}"
+            conf_name = f"{self.molecule.can}_conf_{conf_id}"
             fs_conf_name = f"{self.molecule.fs_name}_conf_{conf_id}"
 
             # coordinates block
@@ -43,7 +46,7 @@ class gaussian_input_generator(object):
             coords_block = "\n".join(map(" ".join, geom_np_array))
 
             # create the gaussian input file
-            self.__generate_file(workflow,
+            self.__generate_file(tasks,
                                  conf_name,
                                  fs_conf_name,
                                  resource_block,
@@ -52,8 +55,9 @@ class gaussian_input_generator(object):
                                  self.molecule.heavy_elements,
                                  self.molecule.mol.GetTotalCharge(),
                                  self.molecule.mol.GetTotalSpinMultiplicity())
+        return tasks
 
-    def __generate_file(self, workflow, name, fs_name, resource_block, coords_block,
+    def __generate_file(self, tasks, name, fs_name, resource_block, coords_block,
                         light_elements, heavy_elements, charge, multiplicity):
 
         heavy_block = ""
@@ -69,12 +73,12 @@ class gaussian_input_generator(object):
         output = ""
 
         # loop through the tasks in the workflow and create input file
-        for i, task in enumerate(workflow.value):
+        for i, task in enumerate(tasks):
 
             if i == 0:  # first task is special, coordinates follow
                 output += resource_block
                 output += f"%Chk={fs_name}_{i}.chk\n"
-                output += f"# {task['route'](self.basis_set)}\n\n"
+                output += f"# {task}\n\n"
                 output += f"{name}\n\n"
                 output += f"{charge} {multiplicity}\n"
                 output += f"{coords_block.strip()}\n"
@@ -84,7 +88,7 @@ class gaussian_input_generator(object):
                 output += resource_block
                 output += f"%Oldchk={fs_name}_{i - 1}.chk\n"
                 output += f"%Chk={fs_name}_{i}.chk\n"
-                output += f"# {task['route'](self.basis_set)}\n"
+                output += f"# {task}\n"
                 output += f"\n"
 
             if self.molecule.heavy_elements:
@@ -96,4 +100,4 @@ class gaussian_input_generator(object):
         with open(file_path, "w") as file:
             file.write(output)
 
-        logger.info(f"Generated a Gaussian input file in {file_path}")
+        logger.debug(f"Generated a Gaussian input file in {file_path}")
