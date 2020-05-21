9b4a2fc51e1789630c38b53fd33b0d65b4206194
zuranski <azuranski@gmail.com>
2020-05-21 19:38:06 -0400

    fixed remote dir for adroit (different than on della)

--- autoqchem/slurm_manager.py
+++ autoqchem/slurm_manager.py
@@ -39,7 +39,7 @@ class slurm_manager(object):
 
         self.host = host
         self.user = user
-        self.remote_dir = f"/scratch/gpfs/{self.user}/gaussian"
+        self.remote_dir = f"/scratch/{'gpfs' if 'della' in host else 'network'}/{self.user}/gaussian"
         self.connection = None
 
     def connect(self) -> None:
