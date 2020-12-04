ae747b55e64219b0ea770d38717e8144eb69d9eb
zuranski <azuranski@gmail.com>
2020-12-04 15:11:04 -0500

    removed obsolete testing module

--- tests/test_molecule.py
+++
@@ -1,9 +0,0 @@
-import os
-import unittest
-
-
-class TestOpenBabel(unittest.TestCase):
-
-    def test_openbabel_datadir(self):
-        ob_datadir = os.environ['BABEL_DATADIR']
-        self.assertTrue(os.path.exists(ob_datadir))
