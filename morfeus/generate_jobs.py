0dab7b5cddee8b70e9061e73785265018895a985
Andrzej Zuranski <zuranski@della5.princeton.edu>
2021-10-04 15:41:28 -0400

    updates to generate jobs script

--- morfeus/generate_jobs.py
+++ morfeus/generate_jobs.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 
-data_dir = "C:/Users/AndrzejZuranski/Dropbox/DataX_PU/Projects/Deoxy/data"
+data_dir = "/home/zuranski"
 input_a = pd.read_csv(f"{data_dir}/smiles.csv")
 input_asf = pd.read_csv(f"{data_dir}/smiles_sulfonate_esters.csv")
 
@@ -14,14 +14,14 @@ input_tot = input_asf
 for _, row in input_tot.iterrows():
     name, smiles = row['name'], row['can']
 
-    job_file = f"{data_dir}/{name}.sh"
+    job_file = f"/tigress/zuranski/jobs/{name}.sh"
 
     file_str = f"#!/bin/bash\n" \
                f"#SBATCH --nodes=1\n" \
                f"#SBATCH --ntasks=1\n" \
                f"#SBATCH --cpus-per-task=4\n" \
                f"#SBATCH --mem-per-cpu=2G\n" \
-               f"#SBATCH --time=01:00:00\n" \
+               f"#SBATCH --time=23:59:00\n" \
                f"\n" \
                f"module purge\n" \
                f"module load anaconda3/2021.5\n" \
@@ -30,12 +30,10 @@ for _, row in input_tot.iterrows():
                f"script=/home/zuranski/auto-qchem/morfeus/morfeus_descriptors.py\n" \
                f"output_path=/tigress/zuranski/jobs\n" \
                f"\n"
-    file_str += f"python $script {smiles} {name} $output_path --n_confs 10 --solvent THF --descriptors " \
-                f"--substructre COS --substructure_labels C 0 S --sterimol_pairs 1,0"
+    file_str += f"python $script '{smiles}' {name} $output_path --n_confs 200 --solvent THF --descriptors " \
+                f"--substructure COS --substructure_labels C 0 S --sterimol_pairs 1,0"
 
-    if name == '1aa_PBSF':
-        print(file_str)
-        with open(job_file, "w") as f:
-            f.write(file_str)
+    with open(job_file, "w") as f:
+        f.write(file_str)
 
-        os.system(f"sbatch {job_file}")
+    os.system(f"sbatch {job_file}")
