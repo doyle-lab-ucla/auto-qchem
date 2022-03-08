14bb41cb19095344f26551f4c9774edf7470a3a7
zuranski <azuranski@gmail.com>
2022-03-08 13:01:36 -0500

    adding thumbnail for social media sharing

--- dash_app/app.py
+++ dash_app/app.py
@@ -3,6 +3,11 @@ import dash_bootstrap_components as dbc
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SANDSTONE],
                 title='Auto-QChem DB',
-                update_title="Loading...")
+                update_title="Loading...",
+                meta_tags=[
+                    {"name": "og image",
+                     "property": "og:image",
+                     "content": "assets/acq_thumb.png"}
+                ])
 server = app.server
-app.config.suppress_callback_exceptions = True 
+app.config.suppress_callback_exceptions = True
